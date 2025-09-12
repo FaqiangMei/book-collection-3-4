0fd61ce61be272b8267b53cfdf51b21b6de1825a
FaqiangMei <faqiang_mei@tamu.edu>
2025-09-11 23:39:54 -0500

    Book4 complete: CRUD + flash + back button + root route

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,13 @@
-# db/seeds.rb
-
-
 UserBook.destroy_all
 User.destroy_all
 Book.destroy_all
 
-
 u1 = User.create!(username: "A")
 u2 = User.create!(username: "B")
-
 
 b1 = Book.create!(title: "Gas", author: "Author1", price: 2, published_date: Date.today)
 b2 = Book.create!(title: "Yes and Not", author: "Author2", price: 5, published_date: Date.today)
 
-
 UserBook.create!(user: u1, book: b1)
 UserBook.create!(user: u2, book: b1)
 UserBook.create!(user: u2, book: b2)
-
-puts "Seeded #{User.count} users, #{Book.count} books, #{UserBook.count} user_books"
